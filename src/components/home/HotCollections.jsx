1c510f046f4c61601b80e5297fe4bbc07f634faf
David <mandeholmes@gmail.com>
2025-04-25 14:33:36 -0400

    Explore Items commit

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -4,29 +4,8 @@ import axios from "axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import SkeletonComponent from './utilities/Skeleton';
-
-const SampleNextArrow = ({ className, style, onClick, icon1 }) => (
-  <div
-    className={className}
-    style={{ ...style, display: "block", background: "black", fontsize: "40px", borderRadius: "50%", color: "white"
-     }}
-    onClick={onClick}
-  >
-    <i className={`fas ${icon1}`}></i>
-  </div>
-);
-
-const SamplePrevArrow = ({ className, style, onClick, icon2 }) => (
-  <div
-    className={className}
-    style={{ ...style, display: "block", background: "black", fontsize: "40px", borderRadius: "50%", color: "white" }}
-    onClick={onClick}
-  >
-    <i className={`fas ${icon2}`}></i>
-  </div>
-);
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import SkeletonComponent from "../utilities/Skeleton";
 
 const HotCollections = () => {
   const [data, setData] = useState([]);
@@ -36,11 +15,20 @@ const HotCollections = () => {
     async function fetchData() {
       try {
         setLoading(true);
-        const response = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections");
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+        );
         setData(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
-        const dummyData = [{ nftImage: "dummyImage1.png", authorImage: "author1.png", title: "Collection 1", code: "001" }];
+        const dummyData = [
+          {
+            nftImage: "dummyImage1.png",
+            authorImage: "author1.png",
+            title: "Collection 1",
+            code: "001",
+          },
+        ];
         setData(dummyData);
       } finally {
         setLoading(false);
@@ -49,11 +37,45 @@ const HotCollections = () => {
     fetchData();
   }, []);
 
+  const SampleNextArrow = ({ className, style, onClick, icon1 }) => (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "black",
+        fontsize: "40px",
+        borderRadius: "50%",
+        color: "white",
+      }}
+      onClick={onClick}
+    >
+      <i className={`fas ${icon1}`}></i>
+    </div>
+  );
+
+  const SamplePrevArrow = ({ className, style, onClick, icon2 }) => (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "black",
+        fontsize: "40px",
+        borderRadius: "50%",
+        color: "white",
+      }}
+      onClick={onClick}
+    >
+      <i className={`fas ${icon2}`}></i>
+    </div>
+  );
+
   const settings = {
     dots: true,
-    infinite: true,  
+    infinite: true,
     speed: 500,
-    slidesToShow: 4,  
+    slidesToShow: 4,
     slidesToScroll: 1,
     initialSlide: 0,
     nextArrow: <SampleNextArrow icon1="fa-solid fa-arrow-right" />,
@@ -65,25 +87,25 @@ const HotCollections = () => {
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 720,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -97,45 +119,45 @@ const HotCollections = () => {
             </div>
           </div>
           <Slider {...settings}>
-            {loading ? (
-              Array(4).fill(0).map((_, index) => (
-                <div className="px-1" key={`skeleton-${index}`}>
-                  <SkeletonComponent />
-                </div>
-              ))
-            ) : (
-              data.map((item, index) => (
-                <div className="px-1" key={index}>
-                  <div className="nft_coll">
-                    <div className="nft_wrap">
-                      <Link to="/item-details">
-                        <img
-                          src={item.nftImage}
-                          className="lazy img-fluid"
-                          alt=""
-                        />
-                      </Link>
+            {loading
+              ? Array(4)
+                  .fill(0)
+                  .map((_, index) => (
+                    <div className="px-1" key={`skeleton-${index}`}>
+                      <SkeletonComponent />
                     </div>
-                    <div className="nft_coll_pp">
-                      <Link to="/author">
-                        <img
-                          className="lazy pp-coll"
-                          src={item.authorImage}
-                          alt=""
-                        />
-                      </Link>
-                      <i className="fa fa-check"></i>
-                    </div>
-                    <div className="nft_coll_info">
-                      <Link to="/explore">
-                        <h4>{item.title || "Pinky Ocean"}</h4>
-                      </Link>
-                      <span>ERC-{item.code}</span>
+                  ))
+              : data.map((item, index) => (
+                  <div className="px-1" key={index}>
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Link to="/item-details">
+                          <img
+                            src={item.nftImage}
+                            className="lazy img-fluid"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Link to="/author">
+                          <img
+                            className="lazy pp-coll"
+                            src={item.authorImage}
+                            alt=""
+                          />
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>{item.title || "Pinky Ocean"}</h4>
+                        </Link>
+                        <span>ERC-{item.code}</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))
-            )}
+                ))}
           </Slider>
         </div>
       </div>
--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -5,8 +5,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import Skeleton from "./utilities/Skeleton";
-
+import Skeleton from "../utilities/Skeleton";
+import CountdownTimer from "../utilities/CountdownTimer";
 
 const NewItems = () => {
   const [data, setData] = useState([]);
@@ -98,51 +98,6 @@ const NewItems = () => {
         },
       },
     ],
-  };
-
-  const CountdownTimer = ({ expiryDate }) => {
-    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-    function calculateTimeLeft() {
-      const difference = new Date(expiryDate) - new Date();
-      let timeLeft = {};
-
-      if (difference > 0) {
-        timeLeft = {
-          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-          minutes: Math.floor((difference / 1000 / 60) % 60),
-          seconds: Math.floor((difference / 1000) % 60),
-        };
-      }
-
-      return timeLeft;
-    }
-
-    useEffect(() => {
-      const timer = setInterval(() => {
-        setTimeLeft(calculateTimeLeft());
-      }, 1000);
-
-      return () => clearInterval(timer);
-    }, [expiryDate]);
-
-    if (!expiryDate) return null;
-
-    return (
-      <div className="de_countdown">
-        {timeLeft.hours || timeLeft.minutes || timeLeft.seconds ? (
-          <div className="clock">
-            <span className="clock-time">
-              {String(timeLeft.hours).padStart(2, "0")}:
-              {String(timeLeft.minutes).padStart(2, "0")}:
-              {String(timeLeft.seconds).padStart(2, "0")}
-            </span>
-          </div>
-        ) : (
-          <div className="expired">Expired</div>
-        )}
-      </div>
-    );
   };
 
   return (
